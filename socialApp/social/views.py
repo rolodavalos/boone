725b99ab9a4fbb59638c025613628f094f2d8017
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-19 17:01:08 -0300

    Update views.py

--- socialApp/social/views.py
+++ socialApp/social/views.py
@@ -8,12 +8,12 @@ from django.utils.crypto import get_random_string
 # Create your views here.
 
 def sendMessage(request):
-  api='fdbd4dc698df7344218dd467936d0a585bc89b7c07135'
+  token='fdbd4dc698df7344218dd467936d0a585bc89b7c07135'
   uid='595991732060'
-  source_uid= get_random_string(length=15)
+  custom_uid= get_random_string(length=15)
   if request.method=='POST':
     form=WhatForm(request.POST)
   else:
     form=WhatForm()
-  return render(request,'social/send.html',{'api':api,'uid':uid,'source_uid':source_uid})  
+  return render(request,'social/send.html',{'token':token,'uid':uid,'custom_uid':custom_uid})  
     
