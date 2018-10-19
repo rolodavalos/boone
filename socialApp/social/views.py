3b8e2ad7221eb19af25a4e0c197439f319e92542
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-19 16:52:03 -0300

    Update views.py

--- socialApp/social/views.py
+++ socialApp/social/views.py
@@ -3,13 +3,17 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from social.forms import WhatForm
+from django.utils.crypto import get_random_string
 
 # Create your views here.
 
 def sendMessage(request):
+  api='fdbd4dc698df7344218dd467936d0a585bc89b7c07135'
+  uid='595991732060'
+  source_uid= get_random_string(length=15)
   if request.method=='POST':
     form=WhatForm(request.POST)
   else:
     form=WhatForm()
-  return render(request,'social/send.html')  
+  return render(request,'social/send.html',{'api':api,'uid':uid,'source_uid':source_uid})  
     
