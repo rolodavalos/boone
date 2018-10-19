b05096559a0b48cbfda25e25b8bf4692bf52f986
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-19 13:41:52 -0300

    Update urls.py

--- socialApp/socialApp/urls.py
+++ socialApp/socialApp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from social.views import views as social_views 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('social/', social_views.show,'show'),
 ]
