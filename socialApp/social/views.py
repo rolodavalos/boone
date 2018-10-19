98f36f4a190f5ad6b8f7d9ceae397877ca9d0839
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-19 19:04:02 -0300

    Update views.py

--- socialApp/social/views.py
+++ socialApp/social/views.py
@@ -20,7 +20,8 @@ def sendMessage(request):
       req = urllib2.Request('https://www.waboxapp.com/api/send/chat', data) 
       response = urllib2.urlopen(req) 
       result = response.read()
-      return render(request,'social/send.html',{'message':result})  
+      message= result.cleaned_data().get("success")
+      return render(request,'social/send.html',{'message':message})  
   else:
     form=WhatForm()
   return render(request,'social/send.html')  
