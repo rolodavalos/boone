6035582d7ceb00534f24f58e39aac3fbcac5fe07
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-19 17:15:36 -0300

    Update views.py

--- socialApp/social/views.py
+++ socialApp/social/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 from social.forms import WhatForm
 from django.utils.crypto import get_random_string
+import urllib, urllib2
 
 # Create your views here.
 
@@ -13,6 +13,13 @@ def sendMessage(request):
   custom_uid= get_random_string(length=15)
   if request.method=='POST':
     form=WhatForm(request.POST)
+    if form.valid():
+      to=form.cleaned_data.get("to")
+      text=form.cleaned_data.get("text")
+      data = urllib.urlencode({"token":token,"uid":uid,"to":to,"custom_uid":custom_uid,"text":text}) 
+      req = urllib2.Request('https://www.waboxapp.com/api/send/chat', data) 
+      response = urllib2.urlopen(req) 
+      result = response.read()
   else:
     form=WhatForm()
   return render(request,'social/send.html',{'token':token,'uid':uid,'custom_uid':custom_uid})  
