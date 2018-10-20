a33b66fc031f7460df64794f56399c73ac5bf6fb
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-20 15:47:34 -0300

    Update models.py

--- socialApp/social/models.py
+++ socialApp/social/models.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Whabox(models.Model):
+	event=models.CharField(max_lenght=255)
+	token=models.CharField(max_lenght=255)
+	contact_uid=models.CharField(max_lenght=255)
+	contact_name=models.CharField(max_lenght=255)
+	contact_type=models.CharField(max_lenght=255)
+	message_dtm=models.CharField(max_lenght=255)
+	message_uid=models.CharField(max_lenght=255)
+	message_cuid=models.CharField(max_lenght=255)
+	message_dir=models.CharField(max_lenght=255)
+	message_type=models.CharField(max_lenght=255)
+	message_body=models.CharField(max_lenght=255)
+	ack=models.CharField(max_lenght=255)
