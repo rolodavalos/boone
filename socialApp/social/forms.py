42a807c25c5d52363184d2bb14ab7afe5815ab24
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-21 20:51:36 -0300

    Update forms.py

--- socialApp/social/forms.py
+++ socialApp/social/forms.py
@@ -1,16 +1,9 @@
 from django import forms
 from social.models import Whabox
-import json
+
 
 class WhatForm(forms.Form):
 	to=forms.CharField(max_length=100)
 	text=forms.CharField(max_length=100)
-	
-class WhaboxForm(forms.Form):
-	jsonfield = forms.CharField(max_length=1024)
-	def clean_jsonfield(self):
-		jdata = self.cleaned_data['jsonfield']
-         	json_data = json.loads(jdata) 
-         	return jdata
 	
 	
