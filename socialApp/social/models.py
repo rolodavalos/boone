cf0ec61f467bb819ee25d5f03c2335eef07f4aa0
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-22 14:57:24 -0300

    Update models.py

--- socialApp/social/models.py
+++ socialApp/social/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Whabox(models.Model):
 	event=models.CharField(max_length=255)
