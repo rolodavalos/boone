a4cbc3831e9aa9643d20f5a13a6adb5991849be1
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-22 14:42:36 -0300

    Update models.py

--- socialApp/social/models.py
+++ socialApp/social/models.py
@@ -19,3 +19,46 @@ class Whabox(models.Model):
 	
 	def __str__(self):
 		return self.event+" "+self.token+" "+self.contact_uid+" "+self.contact_name+" "+self.contact_type+" "+self.contact_name+" "+self.message_dtm+" "+self.message_uid+" "+self.message_cuid+" "+self.message_dir+" "+self.message_type+" "+self.message_text+" "+self.message_ack
+
+	
+class classification(models.Model):
+	title=models.CharFIeld(max_length=59)
+	descrip=models.CharFIeld(max_length=200)
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
+	
+class Conversation(models.Model):
+	STATUS = (
+    	(1, ("open")),
+    	(2, ("process")),
+    	(3, ("close"))
+	)
+	TYPE = (
+    	(1, ("TW")),
+    	(2, ("WA")),
+    	(3, ("FB")),
+	(4, ("IG")),
+	)
+	message_ciud=models.CharField(max_length=255)
+	contact_uid=models.CharField(max_length=255)
+	created=models.DateTimeField(auto_now_add=True)
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
+	estado= models.IntegerField(choices=STATUS)
+	tipo=models.IntegerField(choices=TYPE)
+	
+class Message(models.Model):
+	STATUS = (
+    	(0, ("NOT SEND")),
+    	(1, ("SEND")),
+    	(2, ("DELIVERED")),
+	(3, ("READ")),
+	)
+	conversation=ForeignKey(User,on_delete=models.CASCADE)
+	message_text=uid=models.CharField(max_length=255)
+	estado=models.IntegerField(choices=STATUS)
+	created=models.DateTimeField(auto_now_add=True)
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
+	
+	
+	
+
+	
