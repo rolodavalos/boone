87f379aee5ec976556f49f57cf5ac00f47e112cf
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-23 00:28:41 -0300

    Update views.py

--- socialApp/social/views.py
+++ socialApp/social/views.py
@@ -72,7 +72,7 @@ def hooks(request):
       
       #Guardar mensaje
       message=Message()
-      message.conversation=conversations.first()
+      message.conversation=conversation
       message.message_text=whabox.message_text
       message.estado=whabox.message_ack
       message.save()
@@ -86,13 +86,55 @@ def listMessages(request):
     return render (request,'social/mensajes.html',{'conversations':conversations})
   
 def viewMessage(request,id):
-   conversations=Conversation.objects.all()
-   conversation= Conversation.objects.get(contact_uid=id)
-   messages= Message.objects.filter(conversation__pk=conversation.pk)
-   if request.method=='GET':
-    return render (request,'social/mensajes.html',{'conversations':conversations,'messages':messages,'conversation':conversation}) 
-    
-  
-  
+  conversations=Conversation.objects.all()
+  conversation= Conversation.objects.get(contact_uid=id)
+  messages= Message.objects.filter(conversation__pk=conversation.pk)   
+  return render (request,'social/mensajes.html',{'conversations':conversations,'messages':messages,'conversation':conversation})       
+      
+def save_message(request):
+  if request.method==POST:
+      token='fdbd4dc698df7344218dd467936d0a585bc89b7c07135'
+      uid='595991732060'
+      custom_uid= get_random_string(length=15)
+      form=WhatForm(request.POST)
+        if form.is_valid():
+          conversations= Conversation.objects.filter(contact_uid=to)
+          conversation= Conversation.objects.get(contact_uid=id)
+          to=form.cleaned_data.get("to")
+          text=form.cleaned_data.get("text")
+          
+          #Comprobe messages
+         
+          if conversations:
+            message=Message()
+            message.conversation=conversations.first()
+            message.message_text=text
+            message.estado=3
+            message.save() 
+          else:
+            #Crear nueva conversacion y adjuntar el mensaje
+            conversation=Conversation()
+            conversation.message_cuid=custom_uid
+            conversation.contact_uid=to
+            conversation.estado=1
+            conversation.tipo=2
+            conversation.save()
+      
+            #Guardar mensaje
+            message=Message()
+            message.conversation=conversation
+            message.message_text=whabox.message_text
+            message.estado=whabox.message_ack
+            message.save()
+          
+        #TO WHABOX
+        data = urllib.urlencode({"token":token,"uid":uid,"to":to,"custom_uid":custom_uid,"text":text}) 
+        req = urllib2.Request('https://www.waboxapp.com/api/send/chat', data) 
+        response = urlopen.urlopen(req)       
+        return render (request,'social/mensajes.html',{'conversations':conversations,'messages':messages,'conversation':conversation})       
+          
+          
+          
+              
     
   
