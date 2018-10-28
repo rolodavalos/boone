1757fcf3c27bbd773e02f42f5fc60956d499a5d6
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-28 19:40:06 -0300

    Update ajax.js

--- socialApp/static/js/ajax.js
+++ socialApp/static/js/ajax.js
@@ -27,14 +27,15 @@ $(document).ready(function(){
          }); 
    });
    
-      setInterval(function(){
-           $.ajax({
+   setInterval(updateConversations, 5000); //300000 MS == 5 minutes
+   
+   function updateConversations(){
+      $.ajax({
             url:"/conversations/",  
             success: function(response){
             $("#contacts_content").html(response);    
             },   
-         });                      
-      },5000);
-           
+         });
+   }                                      
 });
 
