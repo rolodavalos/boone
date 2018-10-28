6907d1fa6d5001af4c6c07fef40adedf3aad2804
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-28 17:22:56 -0300

    Update ajax.js

--- socialApp/static/js/ajax.js
+++ socialApp/static/js/ajax.js
@@ -2,7 +2,21 @@ $(document).ready(function(){
    $('#conversations').on('click', 'li', function() {
     console.log('click registered');
     var id = $(this).attr('id');
-    console.log(id);  
-    });                             
+    console.log(id);
+      $.ajax({
+         var parametros = {
+         "conversation_id":id
+         }; 
+         data: parametros,
+         url:"{%url 'display_messages'%}",
+         type: 'post',  
+         beforeSend: function() {
+         $("#messages_container").html("<div class='col-12'><img src='{% static 'img/loading-icon.gif' %}'/><div>");
+         },
+         success: function(response){
+         $("#messages_container").html(response);    
+         },   
+      });    
+   });                             
 });
 
