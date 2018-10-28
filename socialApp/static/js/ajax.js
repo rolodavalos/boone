616c8768af84b35c497e3f8c884f7ba978298be3
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-28 16:50:30 -0300

    Create ajax.js

---
+++ socialApp/static/js/ajax.js
@@ -0,0 +1,16 @@
+function display_message(conversation_id){
+  var parametros = {
+    "conversation_id":conversation_id
+  };
+  $.ajax({
+    data: parametros,
+    url:"{%url 'display_messages'%}",
+    type: 'post',  
+    beforeSend: function() {
+       $("#messages_container").html("<div class='col-12'><img src='{% static 'img/loading-icon.gif' %}'/><div>");
+    },
+    success: function(response){
+      $("#messages_container").html(response);    
+    },
+  }); 
+}
