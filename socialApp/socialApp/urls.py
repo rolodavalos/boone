f2e794fe8f1a90595f594c11a9e30016cfde73af
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-28 20:56:39 -0300

    Update urls.py

--- socialApp/socialApp/urls.py
+++ socialApp/socialApp/urls.py
@@ -21,10 +21,8 @@ from social import views as social_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('demo/',social_views.demo),
-  #  path('mensajes/',social_views.listMessages,name='list_messages'),
     path('mensajes/',social_views.viewMessage,name='view_message'), 
     path('messages/',social_views.displayMessage,name='display_message'),
-  #  path('social/', social_views.sendMessage,name='send_message'),
     path('hooks',social_views.hooks,name='hooks'),
     path('contact/',social_views.showContact,name='show_contact'),
     path('conversations/',social_views.listConversations,name='list_conversations'),
