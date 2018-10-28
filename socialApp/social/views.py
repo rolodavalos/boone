933a02a9b85969236d80e50dc2d730e10eb26d73
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-10-28 20:14:47 -0300

    Update views.py

--- socialApp/social/views.py
+++ socialApp/social/views.py
@@ -108,6 +108,7 @@ def listMessages(request):
 def viewMessage(request):
   conversations=None
   conversations=Conversation.objects.order_by('-modified')
+  conversation=conversation.first()
   if request.method=='POST':
     conversation= Conversation.objects.get(contact_uid=request.POST.get('customer_uid'))
   return render (request,'social/mensajes.html',{'conversations':conversations,'conversation':conversation})       
