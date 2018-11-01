f5e202f33d1b1188d9062f4e3da038d7fa03b9a1
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-11-01 16:06:27 -0300

    Update forms.py

--- socialApp/social/forms.py
+++ socialApp/social/forms.py
@@ -3,6 +3,7 @@ from social.models import Whabox
 
 
 class WhatForm(forms.Form):
+	cid=forms.CharField(max_length=100)
 	destino=forms.CharField(max_length=100)
 	text=forms.CharField(max_length=100)
 	
