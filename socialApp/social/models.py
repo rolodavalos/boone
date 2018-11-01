c20c3d15f67c85b3a09dd1ac84572bc22eb76bfd
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-11-01 15:59:10 -0300

    Update models.py

--- socialApp/social/models.py
+++ socialApp/social/models.py
@@ -10,18 +10,18 @@ import urllib
 import json
 
 class Whabox(models.Model):
-	event=models.CharField(max_length=255)
-	token=models.CharField(max_length=255)
-	contact_uid=models.CharField(max_length=255)
-	contact_name=models.CharField(max_length=255)
-	contact_type=models.CharField(max_length=255)
-	message_dtm=models.CharField(max_length=255)
-	message_uid=models.CharField(max_length=255)
-	message_cuid=models.CharField(max_length=255)
-	message_dir=models.CharField(max_length=255)
-	message_type=models.CharField(max_length=255)
-	message_text=models.CharField(max_length=255)
-	message_ack=models.CharField(max_length=255)
+	event=models.CharField(max_length=255,null=True)
+	token=models.CharField(max_length=255,null=True)
+	contact_uid=models.CharField(max_length=255,null=True)
+	contact_name=models.CharField(max_length=255,null=True)
+	contact_type=models.CharField(max_length=255,null=True)
+	message_dtm=models.CharField(max_length=255,null=True)
+	message_uid=models.CharField(max_length=255,null=True)
+	message_cuid=models.CharField(max_length=255,null=True)
+	message_dir=models.CharField(max_length=255,null=True)
+	message_type=models.CharField(max_length=255,null=True)
+	message_text=models.CharField(max_length=255,null=True)
+	message_ack=models.CharField(max_length=255,null=True)
 	
 	def __str__(self):
 		return self.event+" "+self.token+" "+self.contact_uid+" "+self.contact_name+" "+self.contact_type+" "+self.contact_name+" "+self.message_dtm+" "+self.message_uid+" "+self.message_cuid+" "+self.message_dir+" "+self.message_type+" "+self.message_text+" "+self.message_ack
