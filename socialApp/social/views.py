227c9b8dea519b736120b20a790289da6cb81c2b
rolodavalos <44297347+rolodavalos@users.noreply.github.com>
2018-11-06 17:07:05 -0300

    Update views.py

--- socialApp/social/views.py
+++ socialApp/social/views.py
@@ -132,7 +132,7 @@ def displayMessages4Conversation(request):
 
 def viewDashboard(request):
   conversations=Conversation.objects.filter(user__isnull=True)
-  print("La cantidad de mensajes es: "+conversations.count())
+  print("La cantidad de mensajes es: "+str(conversations.count()))
   return render (request, 'social/supervisor/dashboard.html',{conversations:conversations})
   
   
